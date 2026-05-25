e942841c58cf1e699a5d0d7a8777467660e8bebe
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-05-25 10:40:44 +0800

    tweak: RUNTIME_PORT

--- run.production.bat
+++ run.production.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 set RUNTIME_HOST=0.0.0.0
-set RUNTIME_PORT=8001
+set RUNTIME_PORT=8005
 set ENVIRONMENT=production
 
 uv run uvicorn main:app --port %RUNTIME_PORT% --host %RUNTIME_HOST% --proxy-headers --ssl-keyfile ./certificate/privatekey.pem --ssl-certfile ./certificate/certificate.pem
