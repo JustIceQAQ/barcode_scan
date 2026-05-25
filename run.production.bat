01b32c79e233b88eb55fe168f1a7baabaa87072d
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-05-25 10:39:21 +0800

    tweak: packages

--- run.production.bat
+++ run.production.bat
@@ -5,6 +5,6 @@ set RUNTIME_HOST=0.0.0.0
 set RUNTIME_PORT=8001
 set ENVIRONMENT=production
 
-uvicorn main:app --port %RUNTIME_PORT% --host %RUNTIME_HOST% --proxy-headers --ssl-keyfile ./certificate/privatekey.pem --ssl-certfile ./certificate/certificate.pem
+uv run uvicorn main:app --port %RUNTIME_PORT% --host %RUNTIME_HOST% --proxy-headers --ssl-keyfile ./certificate/privatekey.pem --ssl-certfile ./certificate/certificate.pem
 
 ENDLOCAL
